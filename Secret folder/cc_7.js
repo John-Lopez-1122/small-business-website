7b62ed019b2daf08a29859d63b30f800de99f46b
John-Lopez-1122 <jlopez354@usf.edu>
2026-03-08 15:38:38 -0400

    Website reconstruction part 1, relabeling and scrubbing

---
+++ Secret folder/cc_7.js
@@ -0,0 +1,8 @@
+const headLineInput = document.getElementById("headLineInput")
+const updateButton = document.getElementById("updateButton")
+const ctaHeadline = document.querySelector("h2")
+
+updateButton.addEventListener("click", function() {
+    ctaHeadline.textContent = headLineInput.value;
+}
+);
